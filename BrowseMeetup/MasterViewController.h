a8eaee179334d4a198f92ee02accf22f993541f7
Alexander Kuliev <avkuliev@gmail.com>
2014-10-30 18:26:45 +0400

    Initial Commit

---
+++ BrowseMeetup/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  BrowseMeetup
+//
+//  Created by Alexander Kuliev on 10/30/14.
+//  Copyright (c) 2014 Alexander Kuliev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
